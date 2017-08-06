a0c91e5826c69806bf8316cb5b6c639260d83f13
Carlos Augusto <cardevisi>
2017-08-06 17:16:11 -0300

    adding server.js

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Node.JS!');
-}).listen(8080);
-console.log('Server running at http://localhost:8080/');
+}).listen(5000);
+console.log('Server running at http://localhost:5000/');
